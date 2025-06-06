c228d3e0b0d7455c5ec92ff4cafe84dba74173d5
Bijay-Gurung <stha4580@gmail.com>
2025-06-06 17:07:44 +0545

    Learning Collections on dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,31 @@
-// Learning Flutter
+// Where filter
 
-// Variables and data types
-// var name = "Bijay Gurung";
-
+// Filter only odd number from list
 // void main(){
-//   print(name);
+//     List<int> nums = [1,2,3,4,5,6,7,8,9,10];
+//     var result = nums.where((num) => num%2 != 0);
+//     print(result.toList());
 // }
 
+// Filter days start with 'S'
 // void main(){
-//   /*
-//   const and final keyword is used to declare as constant. variable which has var as a keyword can me manipulated. the value can be changed.
-//   */
-//   final a = 12;
-//   const pi = 3.14;
-//   name = "Karma Gurung";
-//   print(a);
-//   print(pi);
-//   print(name);
+//     List<String> days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+//     List<String> filteredDays = days.where((day) => day.startsWith('S')).toList();
+//     print(filteredDays);
 // }
 
-/* 
-Dart Data types:
-numbers, strings, Booleans, Lists and Maps
-*/
+void Marks(){
+    Map<String, double> marks = {
+        'Ram': 90,
+        'Hari': 80,
+        'Shyam': 50
+    };
 
-// void main(){
-//   var list = {1,2,3,4,5};
-//   print(list);
+    marks.removeWhere((key, value) => value < 60);
+    print(marks);
+}
 
-//   var mapping = {'id':1, 'name': 'Bijay Gurung'};
-//   print(mapping);
-
-//   // Dynamic Data type variable: if the variable type is not defined, then its default type is dynamic.
-//   dynamic name = 'Hero';
-//   print(name);
-// }
-
-// Decision Making and Loops
-// void main(){
-//   // simple for loops to print the even number from 1-10
-//   for(int i=1; i<=10; i++){
-//     if(i%2==0){
-//       print(i);
-//     }
-//   }
-// }
-
-// void main(){
-//   add(5,2);
-// }
-
-// void add(int a, int b){
-//   int result = a+b;
-//   print("The addition of ${a} and ${b} is ${result}.");
-// }
+void main(){
+    Marks();
+}
